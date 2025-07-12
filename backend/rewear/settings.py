b9ee70057239df9d09212cacfeb61f50a8335dab
darunesh1 <sathyarajasekar5873@gmail.com>
2025-07-12 12:32:30 +0530

    swagger is work

--- backend/rewear/settings.py
+++ backend/rewear/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'djoser',
+    'drf_yasg',
     'corsheaders',
 
     # local
--- backend/rewear/urls.py
+++ backend/rewear/urls.py
@@ -4,10 +4,31 @@ from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ReWear API",
+        default_version='v1',
+        description="API documentation for the ReWear app",
+        contact=openapi.Contact(email="team@rewear.dev"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", lambda r: JsonResponse({"status": "rewear backend running"})),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 
     # DJOSER auth endpoints
     path('auth/', include('djoser.urls')),          # user, register, me
