8f8439d5fb3c89e7c862ea56ebb797fbb1fe97ce
ryu-ryuk <meow@alokranjan.me>
2025-07-12 10:20:07 +0530

    feat: user unfold admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from unfold.admin import ModelAdmin 
 from .models import User
 
+
 @admin.register(User)
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(ModelAdmin, BaseUserAdmin):
     model = User
     list_display = ("username", "email", "points", "location", "is_staff")
+
     fieldsets = BaseUserAdmin.fieldsets + (
         ("Profile", {"fields": ("points", "location", "profile_picture")}),
     )
