2a5b2756368c80f5b20b8cac6ac6df10b13e71dd
darunesh1 <sathyarajasekar5873@gmail.com>
2025-07-12 10:43:15 +0530

    removeing login Api

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import login_view
 
 urlpatterns = [
-    path('/login',login_view, name='login-view'),
+    #path('/login',login_view, name='login-view'),
 ]
 
 
